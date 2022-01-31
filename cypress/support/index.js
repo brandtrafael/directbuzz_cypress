5807697221a76eb1629197fa0e0f5c52a88257ca
brandtrafael <rafael.brandt@leadlovers.com.br>
2022-01-31 15:39:22 -0300

    update

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import "./general_commands";
-
+import "./api_commands";
+import "./gui_commands";
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
