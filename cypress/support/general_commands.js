eabf33ea018dd13bf74ff4e4022d5e02b8a8e14e
brandtrafael <rafael.brandt@leadlovers.com.br>
2022-01-31 09:56:01 -0300

    update

--- cypress/support/general_commands.js
+++ cypress/support/general_commands.js
@@ -1,3 +1,5 @@
+const cypress = require("cypress");
+
 Cypress.Commands.add("login", () => {
   cy.visit("/");
   cy.get("[data-test='input-email-login'] > div > input").type(
@@ -8,4 +10,26 @@ Cypress.Commands.add("login", () => {
   );
   cy.get("[data-test='btn-login']").click();
   cy.url().should("be.equal", `${Cypress.config(`baseUrl`)}campaigns`);
+});
+
+Cypress.Commands.add("api_login", () => {
+  cypress.resquest({
+    url: `${Cypress.config("apiUrl")}/auth/login`,
+    method: "POST",
+    body: {
+      email: Cypress.env("registeredUser").username,
+      password: Cypress.env("registeredUser").password,
+    },
+  });
+}).then((res) => res.body.accessToken);
+
+Cypress.Commands.add("api_createCampaign", (type) => {
+  const body = {
+    "sell products": {},
+  };
+  cypress.resquest({
+    url: `${Cypress.config("apiUrl")}/campaigns`,
+    method: "POST",
+    body: {},
+  });
 });
