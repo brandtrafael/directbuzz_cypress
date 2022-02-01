a4bf57e8771126ea8562fd27136170f29f25370f
Luan <luanrodriguesdev@gmail.com>
2022-02-01 16:36:30 -0300

    created test cases 3, 4, 5, 6 and 67

--- cypress/integration/campaigns/createCampaigns.spec.js
+++ cypress/integration/campaigns/createCampaigns.spec.js
@@ -1,6 +1,8 @@
 import campaigns from "../../pages/campaigns/campaigns";
+import variables from "../../pages/campaigns/variables"
 import campaignSellProducts from "../../factories/gui/campaignSellProducts";
 import campaignGenerateContactList from "../../factories/gui/campaignGenerateContactList";
+const faker = require('faker');
 
 describe("Scenario - Functional - Create Project", () => {
   beforeEach(() => cy.login());
@@ -18,18 +20,117 @@ describe("Scenario - Functional - Create Project", () => {
         typeData: "alphaNumeric",
         numberData: 8,
       },
+      {
+        name: "initialMessage",
+        typeData: "words",
+        numberData: 20,
+      },
+      {
+        name: "finalMessage",
+        typeData: "words",
+        numberData: 20,
+      },
+      {
+        name: "emailRequestMessage",
+        typeData: "words",
+        numberData: 20,
+      },
     ]);
     cy.api_createCampaign(campaignData, "generate contact list");
-    cy.get("[data-test='btn-create-new-campaign'").click();
-    cy.get(
-      "[data-test='container-create-campaign-generate-list-onboarding']"
-    ).click();
-    cy.get("[data-test='input-campaign-name-onboarding'] > div > input").type(
-      campaignData.campaignName
-    );
-    cy.get(
-      "[data-test='input-initial-keyword-campaign-onboard'] > div > input"
-    ).type(campaignData.campaignKeyword);
-    cy.get("[data-test='btn-next-step-create-campaign-onboarding']").click();
+    cy.get(variables.btn.newCampaign)
+      .click();
+    cy.get(variables.btn.createCampainGenerateContactList)
+      .click();
+    cy.get(variables.input.campaignName)
+      .type(campaignData.campaignName);
+    cy.get(variables.input.campaignKeyword)
+      .type(faker.random.alphaNumeric(8));
+    cy.get(variables.btn.nextStep)
+      .click();
+    cy.contains("Já existe uma campanha com esse nome")
+    cy.url().should("not.be.equal", `${Cypress.config(`baseUrl`)}campaigns/new/campaign-goal-messages`)
+    cy.visit(Cypress.config('baseUrl'))
+    campaigns.deleteCampaignByName(campaignData.campaignName)
   });
+  it("DIRECTBUZZ-4", () => {
+    const campaignData = campaignGenerateContactList.data([
+      {
+        name: "campaignName",
+        typeData: "alphaNumeric",
+        numberData: 8,
+      },
+      {
+        name: "campaignKeyword",
+        typeData: "alphaNumeric",
+        numberData: 8,
+      },
+      {
+        name: "initialMessage",
+        typeData: "words",
+        numberData: 20,
+      },
+      {
+        name: "finalMessage",
+        typeData: "words",
+        numberData: 20,
+      },
+      {
+        name: "emailRequestMessage",
+        typeData: "words",
+        numberData: 20,
+      },
+    ]);
+    cy.api_createCampaign(campaignData, "generate contact list");
+    cy.get(variables.btn.newCampaign)
+      .click();
+    cy.get(variables.btn.createCampainGenerateContactList)
+      .click();
+    cy.get(variables.input.campaignName)
+      .type(faker.random.alphaNumeric(8));
+    cy.get(variables.input.campaignKeyword)
+      .type(campaignData.campaignKeyword);
+    cy.get(variables.btn.nextStep)
+      .click();
+    cy.contains("Já existe uma campanha com essa palavra-chave")
+    cy.url().should("not.be.equal", `${Cypress.config(`baseUrl`)}campaigns/new/campaign-goal-messages`)
+    cy.visit(Cypress.config('baseUrl'))
+    campaigns.deleteCampaignByName(campaignData.campaignName)
+  });
+  it('DIRECTBUZZ-5', () => {
+    cy.get(variables.btn.newCampaign)
+      .click();
+    cy.get(variables.btn.createCampainGenerateContactList)
+      .click();
+    cy.get(variables.input.campaignKeyword)
+      .type(faker.random.alphaNumeric(8));
+    cy.get(variables.btn.nextStep)
+      .click();
+    cy.contains("Informe um nome para sua campanha.")
+    cy.url().should("not.be.equal", `${Cypress.config(`baseUrl`)}campaigns/new/campaign-goal-messages`)
+  })
+  it('DIRECTBUZZ-6', () => {
+    cy.get(variables.btn.newCampaign)
+      .click();
+    cy.get(variables.btn.createCampainGenerateContactList)
+      .click();
+    cy.get(variables.input.campaignName)
+      .type(faker.random.alphaNumeric(8));
+    cy.get(variables.btn.nextStep)
+      .click();
+    cy.contains("Informe a palavra-chave para sua campanha.")
+    cy.url().should("not.be.equal", `${Cypress.config(`baseUrl`)}campaigns/new/campaign-goal-messages`)
+  })
+  it('DIRECTBUZZ-67', () => {
+    cy.get(variables.btn.newCampaign)
+      .click();
+    cy.get(variables.btn.createCampainGenerateContactList)
+      .click();
+    cy.get(variables.input.campaignName)
+      .type(faker.random.alphaNumeric(8));
+    cy.get(variables.input.campaignKeyword)
+      .type(faker.random.alphaNumeric(8));
+    cy.get(variables.btn.nextStep)
+      .click();
+    cy.get
+  })
 });
