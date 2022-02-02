85019328bb7acc32f3f3f0e334616ac23c2857c0
brandtrafael <rafael.brandt@leadlovers.com.br>
2022-02-02 17:25:05 -0300

    update

--- cypress/integration/accounts/accounts.spec.js
+++
@@ -1,9 +0,0 @@
-describe("Accounts", () => {
-  beforeEach(() => cy.login());
-  it("DIRECTBUZZ-57", () => {
-    cy.get('[data-test="btn-sidemenu-contas"]').click();
-    cy.contains("Nova conta").click();
-    cy.get('[id="email"]').type(Cypress.env("facebookUser").username);
-    cy.get('[id="pass"]').type(Cypress.env("facebookUser").password);
-  });
-});
